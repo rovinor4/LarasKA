fa9f93d83b0ebdf2b81216ec4ad1eab452c2ab0d
Rovino Ramadhani <70685780+rovinor4@users.noreply.github.com>
2025-05-14 15:52:07 +0700

    Update stasiun.go

--- model/stasiun.go
+++ model/stasiun.go
@@ -1,7 +1,49 @@
 package model
 
 type Stasiun struct {
-	Kode    string
-	Nama    string
-	Address string
+	IDStasiun string
+	Nama      string
+	Alamat    string
+}
+
+type Penumpang struct {
+	IDPenumpang string
+	NamaLengkap string
+	NIK         string
+	Kelamin     string
+	Alamat      string
+	TglLahir    string
+	Pekerjaan   string
+	NoHP        string
+	Email       string
+	Pass        string
+}
+
+type Rute struct {
+	IDRute        string
+	StasiunAwal   string
+	Tujuan        string
+	Harga         int
+	Kapasitas     int
+	JamPerjalanan string
+	IDKereta      string
+}
+
+type Kereta struct {
+	NamaKereta string
+	Kelas      string
+}
+
+type Tiket struct {
+	Tanggal         string
+	Jam             string
+	IDPenumpang     string
+	Harga           int
+	IDRute          string
+	NomorTempatDuduk string
+}
+
+type Admin struct {
+	Username string
+	Pass     string
 }
